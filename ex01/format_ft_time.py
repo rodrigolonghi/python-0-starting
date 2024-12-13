84a3108cbc2644c91a783d9850ea3a8e1e1772e6
Rodrigo Felipe Dos Santos Longhi <rodrigo.longhi@mercadolivre.com>
2024-12-13 18:06:51 -0300

    update ex01

--- ex01/format_ft_time.py
+++ ex01/format_ft_time.py
@@ -1,15 +1,11 @@
 import datetime
 
-def main():
-    epoch = datetime.datetime(1970, 1, 1)
-    now = datetime.datetime.now()
-    delta = now - epoch
-    seconds = delta.total_seconds()
+epoch = datetime.datetime(1970, 1, 1)
+now = datetime.datetime.now()
+delta = now - epoch
+seconds = delta.total_seconds()
 
-    formatted_seconds = f"{seconds:,.4f} or {seconds:.2e} in scientific notation"
-    formatted_date = now.strftime("%b %d %Y")
+formatted_seconds = f"{seconds:,.4f} or {seconds:.2e} in scientific notation"
+formatted_date = now.strftime("%b %d %Y")
 
-    print(f"Seconds since January 1, 1970: {formatted_seconds}\n{formatted_date}")
-
-if __name__ == "__main__":
-    main()
+print(f"Seconds since January 1, 1970: {formatted_seconds}\n{formatted_date}")
